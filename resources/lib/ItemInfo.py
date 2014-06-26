119535185d56e5b592140216f922076ef80759f9
faush01 <shaun@bluebit.com.au>
2014-06-27 07:14:57 +1000

    add more info the the info page

--- resources/lib/ItemInfo.py
+++ resources/lib/ItemInfo.py
@@ -31,18 +31,47 @@ class ItemInfo(xbmcgui.WindowXMLDialog):
         userid = downloadUtils.getUserId()
        
         jsonData = downloadUtils.downloadUrl("http://" + server + "/mediabrowser/Users/" + userid + "/Items/" + self.id + "?format=json", suppress=False, popup=1 )     
-        result = json.loads(jsonData)
+        item = json.loads(jsonData)
         
-        id = result.get("Id")
-        name = result.get("Name")
-        image = downloadUtils.getArtwork(result, "Primary")
-        fanArt = downloadUtils.getArtwork(result, "Backdrop")
+        id = item.get("Id")
+        name = item.get("Name")
+        image = downloadUtils.getArtwork(item, "Primary")
+        fanArt = downloadUtils.getArtwork(item, "Backdrop")
         
+        episodeInfo = ""
+        type = item.get("Type")
+        if(type == "Episode" or type == "Season"):
+            name = item.get("SeriesName") + ": " + name
+            season = str(item.get("ParentIndexNumber")).zfill(2)
+            episodeNum = str(item.get("IndexNumber")).zfill(2)
+            episodeInfo = "S" + season + "xE" + episodeNum
+            
         url =  server + ',;' + id
         url = urllib.quote(url)
         self.playUrl = "plugin://plugin.video.xbmb3c/?url=" + url + '&mode=' + str(_MODE_BASICPLAY)
         
+        videoInfo = ""
+        audioInfo = ""
+        
+        mediaStreams = item.get("MediaStreams")
+        if(mediaStreams != None):
+            for mediaStream in mediaStreams:
+                if(mediaStream.get("Type") == "Video" and len(videoInfo) == 0):
+                    videocodec = mediaStream.get("Codec")
+                    height = str(mediaStream.get("Height"))
+                    width = str(mediaStream.get("Width"))
+                    aspectratio = mediaStream.get("AspectRatio")
+                    videoInfo = width + "x" + height + " (" + aspectratio + ") " + videocodec
+                if(mediaStream.get("Type") == "Audio" and len(audioInfo) == 0):
+                    audiocodec = mediaStream.get("Codec")
+                    channels = mediaStream.get("Channels")
+                    audioInfo = audiocodec + " " + str(channels)
+        
+        self.getControl(3202).setLabel(videoInfo)
+        self.getControl(3204).setLabel(audioInfo)
+        
         self.getControl(3000).setLabel(name)
+        self.getControl(3003).setLabel(episodeInfo)
         self.getControl(3001).setImage(fanArt)
         self.getControl(3009).setImage(image)
         
