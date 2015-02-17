f5f19c020e9eabc2c52beb2700431df22ef1f947
angelblue05 <angelblue05@users.noreply.github.com>
2015-02-16 18:04:24 -0600

    deviceId update
    Simplification to avoid generating a new deviceId every time Kodi restart. Converted Client Id term to deviceId to follow the MB header naming.

--- resources/lib/ClientInformation.py
+++ resources/lib/ClientInformation.py
@@ -3,12 +3,42 @@ import xbmc
 import xbmcaddon
 import xbmcgui
 
+addon = xbmcaddon.Addon()
 
 class ClientInformation():
 
     def getMachineId(self):
     
         WINDOW = xbmcgui.Window( 10000 )
+        deviceId = addon.getSetting('deviceId')
+        
+        # Verify if deviceId exists in settings
+        if deviceId == "":
+            
+            xbmc.log("deviceId - > Not found in Settings")
+            
+            # Generate deviceId
+            guid = uuid4()
+            deviceId = str("%012X" % guid).lower()
+            
+            xbmc.log("deviceId - > New deviceId : %s" % deviceId)
+            
+            # Set deviceId to window and addon settings
+            WINDOW.setProperty('deviceId', deviceId)
+            addon.setSetting('deviceId', deviceId)
+        
+        else:
+            
+            xbmc.log("deviceId - > deviceId saved to WINDOW from Settings : %s" % deviceId)
+
+            # deviceId already exists, set to window
+            WINDOW.setProperty('deviceId', deviceId)
+
+        return deviceId
+        
+        
+        # TO BE DELETED ONCE NEW METHOD FULLY IS TESTED - 02/16/2015 - ANGEL
+        """WINDOW = xbmcgui.Window( 10000 )
         
         clientId = WINDOW.getProperty("client_id")
         self.addonSettings = xbmcaddon.Addon(id='plugin.video.xbmb3c')
@@ -27,7 +57,7 @@ class ClientInformation():
                 WINDOW.setProperty('client_id', clientId)
                 xbmc.log("CLIENT_ID - > Client ID saved to WINDOW from Settings : " + clientId)
                 
-        return clientId
+        return clientId"""
         
     def getVersion(self):
         version = xbmcaddon.Addon(id="plugin.video.xbmb3c").getAddonInfo("version")
