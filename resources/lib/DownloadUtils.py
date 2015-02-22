1982d4e0f4f5fc3addec6e193f2e5bc7abea94b9
angelblue05 <angelblue05@users.noreply.github.com>
2015-02-21 21:00:08 -0600

    Update to postcapabilities
    Added SupportsMediaControl to maintain compatibility with new server change.

--- resources/lib/DownloadUtils.py
+++ resources/lib/DownloadUtils.py
@@ -112,8 +112,7 @@ class DownloadUtils():
         supportedCommands = "Play,Playstate,DisplayContent,GoHome,SendString,GoToSettings,DisplayMessage,PlayNext"
         supportMediaControl = True
         
-        url = "http://" + mb3Host + ":" + mb3Port + "/mediabrowser/Sessions/Capabilities?Id=" + sessionId + "&PlayableMediaTypes=" + playableMediaTypes + "&SupportedCommands=" + supportedCommands
-  + "&SupportsMediaControl=" + supportMediaControl       
+        url = "http://" + mb3Host + ":" + mb3Port + "/mediabrowser/Sessions/Capabilities?Id=" + sessionId + "&PlayableMediaTypes=" + playableMediaTypes + "&SupportedCommands=" + supportedCommands + "&SupportsMediaControl=" + supportMediaControl       
         postData = {}
         #postData["Id"] = sessionId;
         #postData["PlayableMediaTypes"] = "Video";
